1fcdd62c9e0a13b69a3f2b813ae838c70b7e4bdb
Chandramouli Gude <chanduchowdary018@gmail.com>
2024-09-22 20:54:26 +0530

    Add files via upload

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -18,10 +18,12 @@ const express = require('express');
 const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
+const ctrLDonation = require('../controllers/shrestitha');
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 /* Other pages */
 router.get('/about', ctrlOthers.about);
+router.get('/donation', ctrLDonation.donation);
 module.exports = router;
